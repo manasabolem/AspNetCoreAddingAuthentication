28be4b3d0de666ff4ea1d4eafcd40d55688bad96
manasabolem <manasa.bolem@gmail.com>
2019-01-12 10:06:56 +0000

    fix

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WishList.Models;
-using WishList.Models.AccountViewModel;
+using WishList.Models.AccountViewModels;
 
 namespace WishList.Controllers
 {
--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WishList.Models.AccountViewModel
+namespace WishList.Models.AccountViewModels
 {
     public class RegisterViewModel
     {
